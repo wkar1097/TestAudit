7a27a756956aa679d5722c4d38ae942796348cc8
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-22 12:26:10 +0530

    "Remove password"

--- src/com/test/TestClass.java
+++ src/com/test/TestClass.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class TestClass {
     public static void main(String[] args) {
-        int password = 12321234;
+        String password = "PASSWORD";
 
         Test test = new Test();
         test.setPassword(password);
