1332275dc5bd4b0e40e833e25f49ee726be03c8b
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-22 12:22:20 +0530

    test

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class TestClass {
+    public static void main(String[] args) {
+        int password = 12321234;
+
+        Test test = new Test();
+        test.setPassword(password);
+    }
+}
