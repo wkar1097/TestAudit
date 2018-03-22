5b98fd7dddb8d1f2b1af5d60b77b882d784fd423
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-22 18:52:15 +0530

    test6

--- src/com/test/TestClass.java
+++ src/com/test/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
         String password = "PASSWORD";
         String erfv = "erfv";
         String aaa = "qws";
+        String aaaaa = "qax";
 
         Test test = new Test();
         test.setPassword(password);
