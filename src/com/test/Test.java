f3a7c1281b725be0a1136cda9e7ed4761139df7d
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-22 13:57:17 +0530

    test

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,11 +1,15 @@
 package com.test;
 
 public class Test {
+    String username;
     String password;
 
     public String getUsername() {
+        return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
