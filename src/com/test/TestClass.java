ed7bec2d178654b0f2d3c34de2a5f3f6f19a694e
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-23 10:21:15 +0530

    commit1

--- src/com/test/TestClass.java
+++ src/com/test/TestClass.java
@@ -3,19 +3,7 @@ package com.test;
 public class TestClass {
     public static void main(String[] args) {
         String username = "newB";
-        String password = "PASSWORD";
-        String erfv = "erfv";
-        String aaa = "qws";
-        String aaaaa = "qax";
-        int item = 13;
-
-        int item2 = 14;
-
-        int item3 = 17;
-
-        int item4 = 16;
-
-
+        String password = "AAAAA";
 
         Test test = new Test();
         test.setPassword(password);
